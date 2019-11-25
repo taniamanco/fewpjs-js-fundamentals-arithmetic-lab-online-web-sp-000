33a18ab4cedfc1376e15ab36bcf56fb526d496a8
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 17:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let num1 = 2;
 let num2 = 31;
 
-let multiplay = num1 * num2;
+let multiply = num1 * num2;
 let random = Math.floor(Math.random() * 1000) + 1;
 let mod = 16 % 6; 
 let max = Math.max(10, 12, 14, 16, 18, 20);
